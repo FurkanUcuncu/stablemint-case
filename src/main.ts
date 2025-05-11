e7bfcd93101f5acc1e8c8426351a829211fc251e
Furkan Ucuncu <furkan.ucuncu_6200@email.ide.builder.ai>
2025-05-11 23:24:57 +0300

    Add tanStack setup

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia';
 import './style.css'
 import App from './App.vue'
+import {QueryClient, VueQueryPlugin} from "@tanstack/vue-query";
 
 const app = createApp(App);
 const pinia = createPinia();
+
+const queryClient = new QueryClient();
+app.use(VueQueryPlugin, { queryClient });
 
 app.use(pinia);
 
